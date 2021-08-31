eb88895bc2654ce72245e1989ea1fc32ea0bce94
Phong Trinh <thanhphongus@gmail.com>
2021-08-31 10:13:23 -0600

    reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,9 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversedInterger = parseInt(n.toString().split('').reverse().join(''));
+  return (n < 0 ? -1 : 1)*reversedInterger;
+}
 
 module.exports = reverseInt;
